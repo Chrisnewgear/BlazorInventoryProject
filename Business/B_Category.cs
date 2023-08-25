1bba76a34e5d71f88c1c8cbf6ad83ac3235c3ac9
Christian Abraham Sánchez Sánchez <csanchez@listosoft.com>
2023-08-25 17:33:59 -0500

    Se agrega la soluciion DataAccess y Buisiness y se crean los metodos para trabajar con la DB.

---
+++ Business/B_Category.cs
@@ -0,0 +1,39 @@
+﻿using Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataAccess;
+
+namespace Business
+{
+    public class B_Category
+    {
+        public List<CategoryEntity> CategoryList()
+        {
+            using (var db = new InventaryContext())
+            {
+                return db.Categories.ToList();
+            }
+        }
+
+        public void CreateCategory(CategoryEntity oCategory)
+        {
+            using (var db = new InventaryContext())
+            {
+                db.Categories.Add(oCategory);
+                db.SaveChanges();
+            }
+        }
+
+        public void UpdateCategory(CategoryEntity oCategory)
+        {
+            using (var db = new InventaryContext())
+            {
+                db.Categories.Update(oCategory);
+                db.SaveChanges();
+            }
+        }
+    }
+}
---
+++ Business/B_Storage.cs
@@ -0,0 +1,39 @@
+﻿using DataAccess;
+using Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business
+{
+    public class B_Storage
+    {
+        public List<StorageEntity> StorageList()
+        {
+            using (var db = new InventaryContext())
+            {
+                return db.Storages.ToList();
+            }
+        }
+
+        public void CreateStorages(StorageEntity oStorage)
+        {
+            using (var db = new InventaryContext())
+            {
+                db.Storages.Add(oStorage);
+                db.SaveChanges();
+            }
+        }
+
+        public void UpdateStorage(StorageEntity oStorage)
+        {
+            using (var db = new InventaryContext())
+            {
+                db.Storages.Update(oStorage);
+                db.SaveChanges();
+            }
+        }
+    }
+}
